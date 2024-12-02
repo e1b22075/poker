519478015b2062d68efa6d4de5795be53066d2a7
e1b22101 <e1b22101@oit.ac.jp>
2024-12-03 01:48:53 +0900

    制限時間の追加(poker.htmlだけ)

--- src/main/java/hakata/poker/PokerApplication.java
+++ src/main/java/hakata/poker/PokerApplication.java
@@ -2,7 +2,11 @@ package hakata.poker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class PokerApplication {
 
